d7030b8560db7500d7a01ca21204de37baf52a93
ruso99 <popp30@naver.com>
2020-05-11 16:18:52 +0900

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+### 파이썬 버전
+3.7.3
+
+### 설치
+python3 -m pip install --user flask
+
+### 실행
+python3 server.py
---
+++ server.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+from flask import Flask, request, jsonify, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/main/test', methods=['POST'])
+def post_test():
+    #받아오는 데이터
+    data = request.get_json()
+
+    #여기서 작업
+
+    #프론트에 보낼 데이터(넘겨야할 데이터를 여기에 담으면 됨)
+    res = { "result" : "성공" }
+    return json.dumps(res,ensure_ascii=False)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0',port=5080)
