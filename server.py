6ab769acf8109bef0ac857a9c63a812d4d1f82d3
jiyoung <dlwldud5148@naver.com>
2020-05-13 18:26:38 +0900

    [ADD]getCompanyList() 추가
    -getStockCode()로부터 회사정보 데이터프레임을 받아, 회사이름과 종목코드를 추출해서 딕셔너리 형태로 반환해줌
    
    Co-Authored-By: Oleg Smirnov <oleg.smirnov@gmail.com>

--- server.py
+++ server.py
@@ -4,12 +4,25 @@ import json
 
 app = Flask(__name__)
 
+def getStockCode():
+
+        url = 'https://dev-kind.krx.co.kr/corpgeneral/corpList.do'
+        df = pd.read_html(url, header=0)[0]
+        df = df[['회사명','종목코드']]
+        df = df.rename(columns={'회사명':'name','종목코드':'code'})
+
+        return df
+
 @app.route('/get/result', methods=['POST'])
 def getResult():
     #받아오는 데이터
     data = request.get_json()
 
     #여기서 작업
+
+    # 받아온 데이터에 해당하는 데이터 크롤링
+
+    # 크롤링한 데이터 딥러닝에 전달
 
     #프론트에 보낼 데이터(넘겨야할 데이터를 여기에 담으면 됨, 무조건 딕셔너리 형태로 담아야함)
     res = { "result" : "성공" }
@@ -20,7 +33,10 @@ def getResult():
 def getCompanyList():
     data = request.get_json()
 
-    res = { "result" : "성공" }
+    # 종목코드를 받아서 딕셔너리 형태로 바꿈({"name":"code", "company1":"code1", "company2":"code2"})
+    stock_cd = getStockCode()
+    stock_cd.to_dict(zip(df['name'], df['code']))
+    res = stock_cd
     return json.dumps(res,ensure_ascii=False)
 
 if __name__ == '__main__':
