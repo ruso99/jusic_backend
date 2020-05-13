47051d2e8879873b4d409d8181cf344dc3877dfd
ruso99 <popp30@naver.com>
2020-05-13 10:51:13 +0900

    [ADD]url추가

--- server.py
+++ server.py
@@ -4,14 +4,22 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/main/test', methods=['POST'])
-def post_test():
+@app.route('/get/result', methods=['POST'])
+def getResult():
     #받아오는 데이터
     data = request.get_json()
 
     #여기서 작업
 
     #프론트에 보낼 데이터(넘겨야할 데이터를 여기에 담으면 됨, 무조건 딕셔너리 형태로 담아야함)
+    res = { "result" : "성공" }
+    return json.dumps(res,ensure_ascii=False)
+
+#페이지 첫 로드시에 가져오는 데이터
+@app.route('/get/company/list', methods=['POST'])
+def getCompanyList():
+    data = request.get_json()
+
     res = { "result" : "성공" }
     return json.dumps(res,ensure_ascii=False)
 
