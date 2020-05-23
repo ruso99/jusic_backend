d21643c848154e01f770cf77ff8daaa8524742b2
jiyoung <dlwldud5148@naver.com>
2020-05-23 16:27:16 +0900

    [ADD] server.py

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
-#-*- coding:utf-8 -*-
 from flask import Flask, request, jsonify, render_template
 import json
+#import getStockData
+import pymysql
+
 
 app = Flask(__name__)
 
 def getStockCode():
-
-        url = 'https://dev-kind.krx.co.kr/corpgeneral/corpList.do'
+        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download'
         df = pd.read_html(url, header=0)[0]
         df = df[['회사명','종목코드']]
         df = df.rename(columns={'회사명':'name','종목코드':'code'})
+        df['code'] = df['code'].astype(str)
+        df['code'] = df['code'].str.zfill(6)
 
         return df
+
 
 @app.route('/get/result', methods=['POST'])
 def getResult():
@@ -20,12 +24,35 @@ def getResult():
 
     #여기서 작업
 
-    # 받아온 데이터에 해당하는 데이터 크롤링
+    #받아온 데이터에 해당하는 데이터 크롤링
+    userName = data["nickname"]
+    companyName = data["companyName"]
+    companyCode = data["companyCode"]
+    toYear = data["toYear"]
+    toMonth= data["toMonth"]
+    toDay = data["toDay"]
+    agencyNetsales = data["agencyNetsales"]
+    foreignerNetsales = data["foreignNetsales"]
+    foreignerSharesheld = data["foreignSharesheld"]
+    usdkrw = data["usdkrw"]
+    jpykrw = data["jpykrw"]
+    cnykrw = data["cnykrw"]
+    kospi = data["kospi"]
+    kosdaq = data["kosdaq"]
+    dji = data["dji"]
+    nas = data["nas"]
+    shs = data["shs"]
+    nii = data["nii"]
+    indicatorOne = data["supplementaryIndicator"]
+    indicatorTwo = data["supplementaryIndicator"]
 
-    # 크롤링한 데이터 딥러닝에 전달
+    #여기서 딥러닝 작업
 
     #프론트에 보낼 데이터(넘겨야할 데이터를 여기에 담으면 됨, 무조건 딕셔너리 형태로 담아야함)
     res = { "result" : "성공" }
+
+    #결과를 DB에 저장
+
     return json.dumps(res,ensure_ascii=False)
 
 #페이지 첫 로드시에 가져오는 데이터
@@ -34,10 +61,40 @@ def getCompanyList():
     data = request.get_json()
 
     # 종목코드를 받아서 딕셔너리 형태로 바꿈({"name":"code", "company1":"code1", "company2":"code2"})
-    stock_cd = getStockCode()
-    stock_cd.to_dict(zip(df['name'], df['code']))
-    res = stock_cd
+    df = getStockCode()
+    res = df.set_index('name').T.to_dict('list')
+    return json.dumps(res,ensure_ascii=False)
+
+#랭킹 페이지
+@app.route('/get/rankinginfos',methods=['POST'])
+def getRanking():
+    #MySQL Connection 연결
+    dbcon = pymysql.connect(host='stockprice.ch9x2a3fnkvg.ap-northeast-2.rds.amazonaws.com', port=3306, user='admin',
+                         passwd='stockprice', db='stock_info', charset='utf8')
+    #Connection 으로부터 Cursor 생성
+    curs = dbcon.cursor()
+
+    #SQL문 실행
+    sql = "select diff_rate,nickname,companyName,toYear,toMonth,toDay,agencyNetsales,foreignNetsales,foreignSharesheld,usdkrw,jpykrw,cnykrw,kospi,kosdaq,dji,nas,shs,nii FROM REQUEST ORDER BY diff_rate DESC"
+    curs.execute(sql)
+
+    #데이터 Fetch
+    rows = curs.fetchall()
+    print(rows)
+    #Connection 닫기
+    dbcon.close()
+
+    #프론트에 보낼 데이터
+    res = rows
+    #return json.dumps(res,ensure_ascii=False)
+    return res
+
+#기록을 전송
+@app.route('/get/record')
+def getRecord():
+    data = request.get_json()
+
     return json.dumps(res,ensure_ascii=False)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',port=5080)
+    app.run(host = '0.0.0.0',port=5000) #이 포트 포워딩하기
